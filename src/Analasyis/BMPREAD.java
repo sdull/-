f14e98de8801899daf41dd445773c1097bc903a5
sdull <asdecwxll@126.com>
2017-11-09 17:47:06 +0800

    文件头读取

--- src/Analasyis/BMPREAD.java
+++ src/Analasyis/BMPREAD.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class BMPREAD {
 
-    private String path = "D://iamges";
-    private int[] imcont;
+    private String path = "";
+    private int[] imconr;
     private int[] bftype = new int[2];
     private int[] bfsize = new int[4];
     private int[] bfoffbits = new int[4];
@@ -20,27 +20,51 @@ public class BMPREAD {
     private int[] biYpelspermeter = new int[4];
     private int[] biclrused = new int[4];
     private int[] biclrimportant = new int[4];
+    private int[] rgbs;
 
     public BMPREAD(String path) throws IOException {
         this.path=path;
-        this.imcont=readbmp();
+        readbmp();
     }
-    public int[] readbmp() throws IOException{
+    public void readbmp() throws IOException{
         java.io.FileInputStream fin = new java.io.FileInputStream(this.path);
         java.io.BufferedInputStream bis = new java.io.BufferedInputStream(fin);
         byte[] imcon = new byte[25600000];
         bis.read(imcon);
-        int[] imconr = new int[imcon.length];
-        for (int i=0;i<imconr.length;i++){
-            if (imconr[i]<0) {
-                imconr[i] = imcon[i]+256;
+        this.imconr = new int[imcon.length];
+        for (int i=0;i<this.imconr.length;i++){
+            if (this.imconr[i]<0) {
+                this.imconr[i] = imcon[i]+256;
             }else{
-                imconr[i] = imcon[i];
+                this.imconr[i] = imcon[i];
             }
         }
         fin.close();
         bis.close();
-        return imconr;
+    }
+    public int dataconvert(int[] type) {
+        int data = type[0] + type[1] << 8 + type[2] << 16 + type[3] << 24;
+        return data;
     }
 
+    public void datainput(){
+        System.arraycopy(this.imconr,0,bftype,0,2);
+        System.arraycopy(this.imconr,2,bfsize,0,4);
+        System.arraycopy(this.imconr,10,bfoffbits,0,4);
+        System.arraycopy(this.imconr,14,bisize,0,4);
+        System.arraycopy(this.imconr,18,biwidth,0,4);
+        System.arraycopy(this.imconr,22,biheight,0,4);
+        System.arraycopy(this.imconr,26,biplanes,0,2);
+        System.arraycopy(this.imconr,28,bibitcount,0,2);
+        System.arraycopy(this.imconr,30,bicompression,0,4);
+        System.arraycopy(this.imconr,34,bisizeimage,0,4);
+        System.arraycopy(this.imconr,38,biXpelspermeter,0,4);
+        System.arraycopy(this.imconr,42,biYpelspermeter,0,4);
+        System.arraycopy(this.imconr,46,biclrused,0,4);
+        System.arraycopy(this.imconr,50,biclrimportant,0,4);
+    }
+
+    public int get_bftype(){
+        return dataconvert(this.bftype);
+    }
 }
