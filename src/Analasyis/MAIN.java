d26b214f1ccd164ce084f6fd117e34701858ad52
sdull <asdecwxll@126.com>
2017-11-12 12:41:27 +0800

    read rgb per pixel

--- src/Analasyis/MAIN.java
+++ src/Analasyis/MAIN.java
@@ -1,8 +1,16 @@
 package Analasyis;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class MAIN {
     public static final void main(String[] args) throws IOException {
     BMPREAD rc =new BMPREAD("D:\\iamges\\33.bmp");
+    HashMap<String,int[][]> clr = rc.get_rgbdata();
+    int clrR =clr.get("clrR")[0][2];
+    int clrG =clr.get("clrG")[0][2];
+    int clrB =clr.get("clrB")[0][2];
+    System.out.println(clrR);
+    System.out.println(clrG);
+    System.out.println(clrB);
     }
 }
